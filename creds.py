6b695aad44ff1e3790e0e8bffda63a0de18ca3f8
Harshal Shah <65274384+harshalshah43@users.noreply.github.com>
2025-08-11 08:48:11 +0530

    Users added
    Siraj and Sagar

--- creds.py
+++ creds.py
@@ -1,6 +1,7 @@
-# Sample user credentials
 USER_CREDENTIALS = {
     "admin": {"password": "admin123", "name": "Admin User"},
     "harshal": {"password": "pass123", "name": "Harshal Shah"},
-    "raj":{"password":"testing101", "name": "Raj Sharma"}
+    "raj":{"password":"testing101", "name": "Raj Sharma"},
+    "siraj":{"password":"testing101", "name": "Siraj Jondhale"},
+    "sagar":{"password":"testing101", "name": "Sagar Jondhale"}
 }
