e9acdf5ceead288b05ee6bf0ea357f35fb1f90ff
Harshal Shah <65274384+harshalshah43@users.noreply.github.com>
2025-10-18 12:26:27 +0530

    Update creds.py
    New User added

--- creds.py
+++ creds.py
@@ -2,5 +2,6 @@ USER_CREDENTIALS = {
     "admin": {"password": "admin123", "name": "Admin User"},
     "harshal": {"password": "pass123", "name": "Harshal Shah"},
     "siraj":{"password":"testing101", "name": "Siraj Jondhale"},
-    "sagar":{"password":"testing101", "name": "Sagar Jondhale"}
+    "sagar":{"password":"testing101", "name": "Sagar Jondhale"},
+    "pratik":{"password":"testing101","name":"Pratik Mudras"}
 }
